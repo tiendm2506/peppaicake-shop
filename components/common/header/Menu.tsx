e2c5602e104b67eb50cba190cf5a686b85e4102b
Minh Tien <minhtien@Minh-Tien-2.local>
2024-04-08 16:19:23 +0700

    Fix active menu

--- components/common/header/Menu.tsx
+++ components/common/header/Menu.tsx
@@ -1,40 +1,38 @@
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 
-import { ROUTES } from './../../../constants'
+import { ROUTES } from '@/constants'
+import { useRouter } from 'next/router'
+
+import styles from './header.module.scss'
 
 export default function Menu() {
-  const pathname = usePathname()
+  const router = useRouter()
+  const { pathname } = router
+  console.log('pathname: ', pathname)
   return (
     <ul>
       <li>
-        <Link href={ROUTES.HOME_PAGE} className={pathname === ROUTES.HOME_PAGE ? 'active' : ''}>
+        <Link href={ROUTES.HOME_PAGE} className={pathname === ROUTES.HOME_PAGE ? styles.active : ''}>
           Trang Chủ
         </Link>
       </li>
       <li>
-        <Link href={ROUTES.ABOUT_PAGE} className={pathname === ROUTES.ABOUT_PAGE ? 'active' : ''}>
+        <Link href={ROUTES.ABOUT_PAGE} className={pathname === ROUTES.ABOUT_PAGE ? styles.active : ''}>
           Về Chúng Tôi
         </Link>
       </li>
       <li>
-        <Link
-          href={ROUTES.PRODUCT_PAGE}
-          className={pathname === ROUTES.PRODUCT_PAGE ? 'active' : ''}
-        >
+        <Link href={ROUTES.PRODUCT_PAGE} className={pathname === ROUTES.PRODUCT_PAGE ? styles.active : ''}>
           Sản Phẩm
         </Link>
       </li>
       <li>
-        <Link href={ROUTES.BLOG_PAGE} className={pathname === ROUTES.BLOG_PAGE ? 'active' : ''}>
+        <Link href={ROUTES.BLOG_PAGE} className={pathname === ROUTES.BLOG_PAGE ? styles.active : ''}>
           Blog
         </Link>
       </li>
       <li>
-        <Link
-          href={ROUTES.CONTACT_PAGE}
-          className={pathname === ROUTES.CONTACT_PAGE ? 'active' : ''}
-        >
+        <Link href={ROUTES.CONTACT_PAGE} className={pathname === ROUTES.CONTACT_PAGE ? styles.active : ''}>
           Liên Hệ
         </Link>
       </li>
